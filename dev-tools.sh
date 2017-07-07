9478c711e68f18a92ecb5f7c7de6679dd042fdfd
Harinder Singh <satnam182@gmail.com>
2017-07-07 11:42:43 +0800

    -y flags
    -y flags on install commands

--- dev-tools.sh
+++ dev-tools.sh
@@ -1,6 +1,6 @@
-sudo apt-get install curl
-sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql php-cli
-sudo apt-get install python-software-properties
+sudo apt-get install -y curl
+sudo apt-get install -y php libapache2-mod-php php-mcrypt php-mysql php-cli
+sudo apt-get install -y python-software-properties
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y apache2
@@ -11,9 +11,9 @@ sudo systemctl restart apache2
 cd ~
 mkdir Tools
 cd Tools
-sudo apt-get install software-properties-common python-software-properties
+sudo apt-get -y install software-properties-common python-software-properties
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
-sudo apt-get install atom
+sudo apt-get -y install atom
 
 
